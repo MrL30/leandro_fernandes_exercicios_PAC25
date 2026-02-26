25290b0c6a373a805dfaceec39f9b33aa8ee03aa
Leandro Fernandes <l30f.2007@gmail.com>
2026-02-26 23:16:06 +0000

    conclusão de exercicios 1 e 2 de match

---
+++ exercicios_match.py
@@ -0,0 +1,35 @@
+#Exercicio 1
+print("\nDias da semana")
+print("\nDomingo (1)")
+print("Segunda (2)")
+print("Terça   (3)")
+print("Quarta  (4)")
+print("Quinta  (5)")
+print("Sexta   (6)")
+print("Sábado  (7)")
+dia_semana = int(input("\nQue dia é hoje? (números de 1 a 7): "))
+def tipo_dia():
+    match dia_semana:
+        case 2 | 3 | 4 | 5 | 6:
+            return print("É um dia útil\n")
+        case 1 | 7:
+            return print("É fim-de-semana\n")
+        case _:
+            return print("Erro...\n")
+tipo_dia()
+
+#Exercicio 2
+print("\n===Calculo de avaliação da nota===")
+nota = float(input("Nota final (De 0 a 100): "))
+def classificacao():
+    match nota:
+        case n if n >= 90:
+            return print("Classificação: Excelente\n")
+        case n if 90 > n >= 70:
+            return print("Classificação: Bom\n")
+        case n if 70 > n >= 50:
+            return print("Classificação: Suficiente\n")
+        case _:
+            return print("Classificação: Insuficiente\n")
+classificacao()
+
