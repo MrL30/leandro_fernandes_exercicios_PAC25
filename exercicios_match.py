6d1ea8ea85a2fbf12daa86341cebc77f5eb410f0
Leandro Fernandes <l30f.2007@gmail.com>
2026-02-27 14:36:38 +0000

    resolução do exercicio 3 de match

--- exercicios_match.py
+++ exercicios_match.py
@@ -33,3 +33,19 @@ def classificacao():
             return print("Classificação: Insuficiente\n")
 classificacao()
 
+#Exercicio 3
+print("\n===Tipo de pedido===")
+tipo = int(input("Pretende vender(1) ou comprar(2) (1/2): "))
+valor = int(input("Preço do produto (em euros): "))
+def tp(escolha):
+    match escolha:
+        case 1:
+            return "venda"
+        case 2:
+            return "compra"
+        case _:
+            return print("Erro...")
+resultado_tipo = tp(tipo)
+escolha = {"tipo":resultado_tipo, "valor":valor}
+print(f"A sua escolha: {escolha}")
+print(f"Quer fazer uma {resultado_tipo} de {valor}€\n")
