0281870b4c18cabbb132e23dfcbf47d2f3340af7
Leandro Fernandes <l30f.2007@gmail.com>
2026-03-11 17:17:29 +0000

    Primeiros exercicios de dicionários

---
+++ exercicios_dicionario.py
@@ -0,0 +1,17 @@
+# #Exercicio 1
+# nome = input("O seu nome: ")
+# idade = input("A sua idade: ")
+# curso = input("O seu curso: ")
+# alunos = {"nome":nome, "idade":idade, "curso":curso}
+
+# print(f"\nnome: {alunos["nome"]}")
+# print(f"idade: {alunos["idade"]}")
+# print(f"curso: {alunos["curso"]}")
+
+# #Exercicio 2
+# carro = {'marca': 'Toyota', 'modelo': 'Corolla', 'ano': 2020}
+
+# print(f"\nModelo do carro: {carro['modelo']}\n")
+
+#Exercicio 3
+vazio = {}
